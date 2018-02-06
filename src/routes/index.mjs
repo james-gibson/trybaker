1f92b157674cb0599e55bddec9376c85bf64ff6e
James Gibson <iam@jamesgibson.co>
2018-02-05 19:24:28 -0700

    Preparing for swagger

--- src/routes/index.mjs
+++ src/routes/index.mjs
@@ -1,10 +1,19 @@
 import express from 'express';
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
+const swaggerDefinition = {
+
+};
+
+/* Health check route */
+router.get('/', function(req, res) {
   res.statusCode = 204;
   res.send();
 });
+
+router.get('/swagger', function(req, res) {
+    res.json(swaggerDefinition);
+});
+
 
 export default router;
