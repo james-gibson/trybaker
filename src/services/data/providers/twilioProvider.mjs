a26d6fc089c4b63365e6a96b8775d5dc567aa35f
James Gibson <iam@jamesgibson.co>
2018-02-05 19:23:07 -0700

    Create twilio provider

---
+++ src/services/data/providers/twilioProvider.mjs
@@ -0,0 +1,33 @@
+//@flow
+import BaseProvider from './baseProvider.mjs';
+import PROVIDERS from "./providerEnum.mjs";
+
+import Twilio from 'twilio';
+
+const appPhoneNumber = '+17206059279';
+
+const sendSMS = twilio => (phoneNumber:string) => (message:string) => twilio.messages.create({
+    from: appPhoneNumber,
+    body: message,
+    to: phoneNumber
+})
+
+
+class TwilioProvider extends BaseProvider {
+    key:typeof PROVIDERS.TWILIO;
+    service: *;
+
+    constructor() {
+        super();
+        this.key = PROVIDERS.TWILIO;
+
+        const twilio = new Twilio(process.env.T_ACCOUNT_ID, process.env.T_AUTH_TOKEN);
+
+        this.service = {
+            sendSMS: sendSMS(twilio)
+        };
+        //sendSMS(twilio)('4439073453')('hello world');
+    }
+}
+
+export default TwilioProvider;
